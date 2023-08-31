f14169ee1ba9d6cba85e091523fce742bd4009fb
TaeHeum <69237887+xogma@users.noreply.github.com>
2023-09-01 02:50:55 +0900

    [Bronze II] Title: 공 바꾸기, Time: 236 ms, Memory: 18512 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int number = sc.nextInt();
+		int change = sc.nextInt();
+		
+		int[] basket = new int[number+1];
+		for(int i = 0; i <= number; i++) {
+			basket[i] = i;
+		}
+		
+		int temp = 0;
+		
+		for(int i = 0; i < change; i++) {
+			int num1 = sc.nextInt();
+			int num2 = sc.nextInt();
+			
+			temp = basket[num1];
+			basket[num1] = basket[num2];
+			basket[num2] = temp;
+			
+		}
+		
+		for(int i = 1; i <= number; i++) {
+			System.out.print(basket[i]+ " ");
+		}
+		
+	}
+
+}
